7195e9bd69b772a4ee372af3b747f2f3a754c89d
Baro-coder <b.szykula00@gmail.com>
2023-12-08 20:44:40 +0100

    Platform client : store data
    Storing actual candles in csv

--- platform-client/app/config/config.py
+++ platform-client/app/config/config.py
@@ -1,8 +1,16 @@
+import os
 from enum import Enum
 
+
+DATA_DIR = 'data'
 
 ROOT_URL = 'https://platform.the-brawl.eu'
 API_URL = f'{ROOT_URL}/ui/api'
 
 class Enpoints(Enum):
     RATES = f'{API_URL}/rates'
+
+
+if not os.path.isdir(DATA_DIR):
+    print('[*] Created data directory: ' + DATA_DIR)
+    os.mkdir(DATA_DIR)
---
+++ platform-client/app/utils/store.py
@@ -0,0 +1,42 @@
+import os
+import csv
+
+from models.schemas import Candle
+
+def __init_file(file_path: str) -> Candle | None:
+    if not os.path.isfile(file_path):
+        print(f'[*] CSV File initialized: {file_path}')
+        with open(file_path, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['time', 'open', 'high', 'low', 'close'])
+        
+        return None
+    
+    else:
+        last_candle : Candle | None = None
+        
+        with open(file_path, mode='r') as file:
+            reader = csv.reader(file)
+            next(reader, None)  
+            for row in reader:
+                if row:
+                    last_candle = Candle(time=int(row[0]), open=float(row[1]), high=float(row[2]), low=float(row[3]), close=float(row[4]))
+        
+        return last_candle
+            
+        
+
+def __write_to_file(file_path: str, candle: Candle) -> None:
+    with open(file_path, mode="a", newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([candle.time, candle.open, candle.high, candle.low, candle.close])
+        print(f'     [+] Row added in [{file_path}] : [{candle.time}, {candle.open}, {candle.high}, {candle.low}, {candle.close}]')
+        
+
+def store_in_csv(file_path: str, candle: Candle) -> None:
+    __init_file(file_path)
+    __write_to_file(file_path, candle)
+
+
+def get_latest_from_csv(file_path: str) -> Candle | None:
+    return __init_file(file_path)
