b3e0cd926a8308afc8bbfe6d545e9d65b4de65c7
Baro-coder <b.szykula00@gmail.com>
2023-12-09 00:09:22 +0100

    Trades operations

--- platform-client/app/core/reqs/trades.py
+++ platform-client/app/core/reqs/trades.py
@@ -1,17 +1,57 @@
 import requests
+from typing import NamedTuple
 
-from core.models.enums import CurrencyId
 from core.models.wallets import Wallet, CryptoWallet
 
 
-def __perform_transaction(wallet_src: Wallet, wallet_dst: Wallet, amount: float):
-    pass
+class TransactionPayload(NamedTuple):
+    sourceWalletId          : str
+    destWalletId            : str
+    amountFromSourceWallet  : float
+    exchangeRate            : str
+
+
+def __perform_transaction(url: str, wallet_src: Wallet, wallet_dst: Wallet, amount: float, cookies: dict) -> None:
+    # Payload build
+    payload = TransactionPayload(
+        sourceWalletId          = wallet_src.id,
+        destWalletId            = wallet_dst.id,
+        amountFromSourceWallet  = amount,
+        exchangeRate            = "0"
+    )
+    
+    # Perform request
+    print('        [$] REQUEST (POST): ' + url, end=' | ')
+    response = requests.post(
+        url=url,
+        data=payload,
+        cookies=cookies,
+        verify=False
+    )
+    
+    # Check response code
+    if response.status_code == 200:
+        print('OK')
+    
+    else:
+        # HTTP Exception
+        print('ERROR - ' + str(response.status_code))
+        print(f'ERROR ({str(response.status_code)})')
+        raise Exception("Error response code: " + str(response.status_code))
 
 
 
-def buy(wallet_src: Wallet, wallet_dst: CryptoWallet, amount: float):
-    pass
+def buy(url: str, wallet_src: Wallet, wallet_dst: CryptoWallet, amount: float, cookies: dict) -> None:
+    try:
+        # Perform buy transaction
+        __perform_transaction(url, wallet_src, wallet_dst, amount, cookies)
+    except Exception:
+        raise
 
 
-def sell(wallet_src: Wallet, wallet_dst: CryptoWallet, amount: float):
-    pass
+def sell(url: str, wallet_src: CryptoWallet, wallet_dst: Wallet, amount: float, cookies: dict) -> None:
+    try:
+        # Perform sell transaction
+        __perform_transaction(url, wallet_src, wallet_dst, amount, cookies)
+    except Exception:
+        raise
